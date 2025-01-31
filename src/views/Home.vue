e5eb47fc5413048d56a0027d4eb70203dec816b3
LEO LIU <leoliu5550@gmail.com>
2025-01-31 12:27:53 +0800

    003

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>Welcome to My Resume</h1>
+    <h1>Welcome to My  Resume</h1>
     <p>I am a data scientist specializing in AI and machine learning.</p>
   </div>
 </template>
